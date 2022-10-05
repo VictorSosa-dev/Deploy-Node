de202d6f8782ffbc6a967601e0fd3a769e0e90ab
VictorSosa-dev <manuelsosapia@gmail.com>
2022-10-04 20:12:42 -0500

    first commit

---
+++ routes/gods.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const {
+    getGod,
+    getGods,
+    createGod,
+    updateGod,
+    deleteGod
+} = require('../controllers/gods')
+
+const auth = require('../config/auth.js');
+
+
+router.get('/',getGods);
+router.get('/:id',getGod);
+router.post('/', auth.required, createGod);
+router.patch('/:id',auth.required, updateGod);
+router.delete('/:id', auth.isAdmin, deleteGod);
+
+module.exports = router;
