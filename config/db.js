063bf238755efa4a15eb6f02146f4fe5a04878c3
VictorSosa-dev <manuelsosapia@gmail.com>
2022-10-04 23:59:56 -0500

    config db

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-    process.env['DATABASE_URL'],
-    process.env['DATABASE_USER'],
-    process.env['DATABASE_PASSWORD'],
-    {
-        host: process.env['DATABASE_HOST'],
-        dialect: process.env['DATABASE_DIALECT']
-    });
-
-module.exports = sequelize;
+if(process.env['NODE_ENV'] === 'production'){
+    module.exports = new Sequelize(process.env['DATABASE_URL']);
+}else{
+    module.exports =  new Sequelize(
+        process.env['DATABASE_URL'],
+        process.env['DATABASE_USER'],
+        process.env['DATABASE_PASSWORD'],
+        {
+            host: process.env['DATABASE_HOST'],
+            dialect: process.env['DATABASE_DIALECT']
+        });
+}
